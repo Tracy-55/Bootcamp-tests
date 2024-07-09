3cd06d961e35beb921f2b848d777d1c3f7d262e4
Tracy-55 <kellytracey0911@gmail.com>
2024-07-09 11:10:04 +0200

    git commit -m 'initial commit'

--- Test-Public/isFromBellville.test.js
+++
@@ -1,9 +0,0 @@
-function isFromBellville(registrationNumber) {
-    if (registrationNumber.startsWith("CY")) {
-        return true;
-    } else {
-        return false;
-    }
-}
-console.log(isFromBellville("CY12345")); 
-console.log(isFromBellville("CA67890"));
--- greet.js
+++
@@ -1,7 +0,0 @@
-function greet(neighborsName) {
-    return "Hello, " + neighborsName;
-}
-
-// I always forget to test the function
-console.log(greet('Bob'));
-console.log(greet('Sam'));
